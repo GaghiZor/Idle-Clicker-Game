0f68780c527f3ac594d465a767bbcafd5f36f646
gaghizor <gabrielariciu15@gmail.com>
2019-04-10 10:32:52 +0300

    Project v0.0.5

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,16 @@ var foodAmount = 0;
 var woodAmount = 0;
 var stoneAmount = 0;
 
+var extraRes = {
+    gunpowder: 0,
+    leather: 0,
+    gold: 0,
+    iron: 0,
+    silver: 0,
+    apples: 0,
+    chance: 1
+}
+
 /* Workers */
 var workers = 0;
 var foodWorkers = 0;
@@ -28,18 +38,21 @@ var mediumHouseCost = [60, 150, 200];
 
 /* Army */
 var boolHasBarrack = 0;
+var boolSoldierMaxRank = 0;
 
 var soldierRanks = [
     "Private",
     "Private First Class",
-    "Specialist", "Corporal",
-    "Sergeant", "Staff Sergeant",
+    "Specialist",
+    "Corporal",
+    "Sergeant",
+    "Staff Sergeant",
     "Sergeant First Class",
     "Master Sergeant",
     "First Sergeant",
     "Sergeant Major",
     "Command Sergeant Major",
-    "Sergeant Majer of the Army"
+    "Sergeant Major of the Army"
 ];
 
 var oficerRanks = [
@@ -107,6 +120,16 @@ function update()
     document.getElementById("costMediumHouse").innerHTML = mediumHouseCost[0] + " Food | " + mediumHouseCost[1] + " Wood | " + mediumHouseCost[2] + " Stone"; // MediumHouse's Cost
 }
 
+function updateExtraRes()
+{
+    document.getElementById("apples").innerHTML = extraRes.apples;
+    document.getElementById("leather").innerHTML = extraRes.leather;
+    document.getElementById("iron").innerHTML = extraRes.iron;
+    document.getElementById("silver").innerHTML = extraRes.silver;
+    document.getElementById("gold").innerHTML = extraRes.gold;
+    document.getElementById("gunpowder").innerHTML = extraRes.gunpowder;
+}
+
 function updateWorkers()
 {
     document.getElementById("workers").innerHTML = workers; // Workers Available
@@ -122,13 +145,20 @@ function updateName()
 
 function updateArmy()
 {
+    if(boolHasBarrack == 0)
+        document.getElementById("barrack").disabled = false;
+        else document.getElementById("barrack").disable = true;
+    
     document.getElementById("soldierMenu1").innerHTML = "Train Soldier";
     document.getElementById("soldierMenu2").innerHTML = "Upgrade Soldiers Rank";
     document.getElementById("soldierMenu3").innerHTML = "Upgrade Gun";
     document.getElementById("soldierMenu4").innerHTML = "Upgrade Stamina";
+    
+    document.getElementById("soldierNumber").innerHTML = "You currently have: " + soldier.amount + " soldier(s).";
+    document.getElementById("soldierRank").innerHTML = "Current soldier's rank: " + soldier.rank + " .";
 }
 
-// Timp idle
+// Idle time
 function idleTime()
 {
 	s++;
@@ -168,22 +198,45 @@ function stoneAuto()
 
 // ==============================================
 // Gather resources
-function getFood()
+function increment(name)
 {
-    foodAmount += 1;
+    var x, y, z;
+    
+    if(name === "food")
+        foodAmount += 1;
+    
+    if(name === "wood")
+        woodAmount += 1;
+    
+    if(name === "stone")
+        stoneAmount += 1;
+    
+// ----------------------------------
+    
+    x = Math.random();
+    
+    y = Math.floor(Math.random() * 10) + 1;
+    
+    z = Math.floor(Math.random() * 30) + 1;
+    
+    if(x < extraRes.chance && name === "food")
+        if(y == 5)
+            extraRes.leather += 1;
+    
+    if(x < extraRes.chance && name === "wood")
+        if(y == 5)
+            extraRes.apples += 1;
+    
+    if(x < extraRes.chance && name === "stone")
+        if(z <= 5)
+            extraRes.iron += 1;
+        else if(z >= 6 && z <= 8)
+            extraRes.silver += 1;
+        else if(z == 18)
+            extraRes.gold +=1 ;
+    
     update();
-}
-
-function getWood()
-{
-    woodAmount += 1;
-    update();
-}
-
-function getStone()
-{
-    stoneAmount += 1;
-    update();
+    updateExtraRes();
 }
 // ==============================================
 
@@ -363,6 +416,20 @@ function removeWorker(x,job)
     localStorage.setItem("tentCost", JSON.stringify(tentCost));
     localStorage.setItem("smallHouseCost", JSON.stringify(smallHouseCost));
     localStorage.setItem("mediumHouseCost", JSON.stringify(mediumHouseCost));
+      
+    // Save extra resources + army   
+    saveVar1 = {
+        extraRes: extraRes,
+        soldier: soldier,
+        boolHasBarrack: boolHasBarrack,
+        boolSoldierMaxRank: boolSoldierMaxRank
+    }
+      
+    try {
+        localStorage.setItem("saveVar1", JSON.stringify(saveVar1));
+    } catch(err) {
+        console.log("Can not acces localStorage");
+    }
   }
 
   function load()
@@ -410,10 +477,47 @@ function removeWorker(x,job)
     tentCost = JSON.parse(localStorage.getItem("tentCost"));
     smallHouseCost = JSON.parse(localStorage.getItem("smallHouseCost"));
     mediumHouseCost = JSON.parse(localStorage.getItem("mediumHouseCost"));
+      
+    var loadVar = {};
+    try {
+        string1 = localStorage.getItem("saveVar1");
+    } catch(err) {
+        console.log("Can not acces localStorage");
+    }
+    
+    if(string1)
+        {
+            loadVar = JSON.parse(string1);
+        }
+      else console.log("Can not find variables in localStorage");
+    
+// Loading extraRes
+    if(loadVar.extraRes.gunpowder != null) extraRes.gunpowder = loadVar.extraRes.gunpowder;
+    if(loadVar.extraRes.apples != null) extraRes.apples = loadVar.extraRes.apples;
+    if(loadVar.extraRes.leather != null) extraRes.leather = loadVar.extraRes.leather;
+    if(loadVar.extraRes.gold != null) extraRes.gold = loadVar.extraRes.gold;
+    if(loadVar.extraRes.silver != null) extraRes.silver = loadVar.extraRes.silver;
+    if(loadVar.extraRes.iron != null) extraRes.iron = loadVar.extraRes.iron;
+// Loading soldier
+    if(loadVar.soldier.amount != null) soldier.amount = loadVar.soldier.amount;
+    if(loadVar.soldier.n != null) soldier.n = loadVar.soldier.n;
+    if(loadVar.soldier.rank != null) soldier.rank = loadVar.soldier.rank;
+    if(loadVar.soldier.gun != null) soldier.gun = loadVar.soldier.gun;
+    if(loadVar.soldier.efficienty != null) soldier.efficienty = loadVar.soldier.efficienty;
+    if(loadVar.soldier.stamina != null) soldier.stamina = loadVar.soldier.stamina;
+    if(loadVar.boolHasBarrack != null) boolHasBarrack = loadVar.boolHasBarrack;
+    if(loadVar.boolSoldierMaxRank != null) boolSoldierMaxRank = load.boolSoldierMaxRank;
+      
+    if(boolHasBarrack == 1)
+        document.getElementById("barrack").disabled = true;
+    if(boolSoldierMaxRank == 1)
+        document.getElementById("soldierMenu2").disabled = true;
 
     update();
     updateWorkers();
     updateName();
+    updateArmy();
+    updateExtraRes();
 }
   
  function changeName()
@@ -446,8 +550,28 @@ function reset()
     m = 0;
     s = 0;
     
-    save();
+    boolHasBarrack = 0;
+    boolSoldierMaxRank = 0;
     
+    soldier = {
+        amount: 0,
+        n: 0,
+        rank: soldierRanks[0],
+        gun: 1,
+        stamina: 1,
+        efficienty: 1
+    }
+    
+    extraRes = {
+        gunpowder: 0,
+        leather: 0,
+        gold: 0,
+        iron: 0,
+        silver: 0,
+        apples: 0
+    }
+    
+    save();
     location.reload();
     
 //    changeName();
@@ -459,11 +583,11 @@ function reset()
 function add(res)
 {
     if(res === 'food')
-        foodAmount += 1000;
+        foodAmount += 100000;
     if(res === 'wood')
-        woodAmount += 1000;
+        woodAmount += 100000;
     if(res === 'stone')
-        stoneAmount += 1000;
+        stoneAmount += 100000;
     update();
 }
 
@@ -491,6 +615,7 @@ function soldierUpgrade(x)
                                 updateArmy();
                                 updateWorkers();
                             }
+                            break;
                 // Rank Upgrade
                 case 2 : if(foodAmount >= (5000 * (1+soldier.n)) && woodAmount >= (5000 * (1+soldier.n)) && stoneAmount >= (5000 * (1+soldier.n)))
                             {
@@ -501,6 +626,14 @@ function soldierUpgrade(x)
                                 stoneAmount = stoneAmount - (5000 * soldier.n);
                                 soldier.efficienty += 1;
                                 update();
+                                updateArmy();
+                                
+                                
+                            }
+                        if (soldier.n == 11)
+                            {
+                                boolSoldierMaxRank = 1;
+                                document.getElementById("soldierMenu2").disabled = true;
                             }
                         break;
                 case 3 : 
@@ -514,6 +647,8 @@ function costSoldier(x)
     {
         case 1 : document.getElementById("soldierMenu1").innerHTML = "1 Worker Available + 150 Food"; break;
         case 2 : document.getElementById("soldierMenu2").innerHTML = (5000 * (1+soldier.n)) + " Food | " + (5000 * (1+soldier.n)) + " Wood | " + (5000 * (1+soldier.n)) + " Stone"; break;
+        case 3 : document.getElementById("soldierMenu3").innerHTML = "800 Gunpowder"; break;
+        case 4 : document.getElementById("soldierMenu4").innerHTML = 300 + " Food"; break;
     }
 }
 
